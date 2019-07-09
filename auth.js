affac0b196500f7d4c06089b5486574c788919b1
makacies <barberrywork@gmail.com>
2019-07-09 22:34:07 +0100

    Update config functionality

--- auth.js
+++ auth.js
@@ -1,16 +1,23 @@
 class Auth {
-    constructor(permissions) {
-        this.permissions = permissions;
+    constructor(permissionsConfig) {
+        const permissions = JSON.parse(permissionsConfig);
+        this.permissions = permissions.map(permission => {
+            const keys = Object.keys(permission);
+            return ({
+                rule: keys[0].replace('AUTH_', '').replace('_FIELD', '').replace('_MATCH', ''),
+                field: permission[keys.find(k => k.includes("FIELD"))],
+                match: permission[keys.find(k => k.includes("MATCH"))]
+            })
+        });
     }
 
     hasAccess(rule, user) {
         const accessCondition = this.permissions.find(item => item.rule === rule);
-        const regExp = new RegExp(accessCondition.match, "g"); 
-        const hasAccess = user[accessCondition.field].match(regExp);
-        return (hasAccess && hasAccess.length === 1);
+        const regExp = new RegExp(accessCondition.match, "g");
+        return regExp.test(user[accessCondition.field])
     };
 }
-    
+
 module.exports = {
     Auth
 };
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const statisticCollector = require('./statisticCollector.js');
-const permissionsConfig = require('./resources/permissions.json');
 const user = require('./resources/user.json');
+const { ENVIRONMENT_VARIABLE, VIEW_PHOTOS } = require('./helpers/constants.js');
 const { Auth } = require('./auth.js');
 
 const statistic = statisticCollector.getPostsFrequency();
-
 console.log(statistic.postsPerDay);
 console.log(statistic.postsPerWeek);
 console.log(statistic.postsPerMonth);
 
-const authenticator = new Auth(permissionsConfig); 
-console.log(authenticator.hasAccess('VIEW_PHOTOS', user));
+//environment variable example could be found in the permissions.json file
+const authenticator = new Auth(process.env[ENVIRONMENT_VARIABLE]);
+console.log(authenticator.hasAccess(VIEW_PHOTOS, user));
